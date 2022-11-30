1265350264c7bb320506e2ccc5a27d4be9b79944
JulioXC4 <danielc4nt@gmail.com>
2022-11-30 16:47:47 -0500

    api progress

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,48 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    health: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    height: {
+      type : DataTypes.FLOAT,
+      allowNull: true
+    },
+    weight: {
+      type : DataTypes.FLOAT,
+      allowNull: true
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  },
+  {
+    timestamps: false
+  }
+  );
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,134 @@
 const { Router } = require('express');
+const {Pokemon, Type} = require('../db.js');
+const axios = require('axios');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
 const router = Router();
-
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const getInitialApiInfo = async() => {
+    const apiUrl = await axios.get('https://pokeapi.co/api/v2/pokemon')
+    const apiData = await apiUrl.data
+    const pokemons = []
+
+    const data = {
+        next: apiData.next,
+        pokemons: pokemons
+    }
+
+    await apiData.results.map(e => {
+        pokemons.push({
+            name: e.name,
+            url: e.url
+        })
+    })
+
+    data.pokemons = pokemons
+
+    return data
+}
+
+const getNextInfo = async(next, pokemons) => {
+    const nextUrl = await axios.get(next)
+    const nextData = await nextUrl.data
+    const newPokemons = []
+
+    const newData = {
+        next: nextData.next,
+        pokemons: newPokemons
+    }
+
+    await nextData.results.map(e => {
+        newPokemons.push({
+            name: e.name,
+            url: e.url
+        })
+    })
+
+    newData.pokemons = await pokemons.concat(newPokemons)
+    newData.next = nextData.next
+
+    return newData
+}
+
+const getAllPokemons = async() => {
+    const initialData = await getInitialApiInfo()
+    const nextData = await getNextInfo(initialData.next,initialData.pokemons)
+    
+    const pokemons = await nextData.pokemons.map(async (e) => {
+        const pokemonUrl = await axios.get(e.url)
+        const pokemonStatsData = await pokemonUrl.data
+        let stats = []
+
+        await pokemonStatsData.stats.map(e => {
+            stats.push(e.base_stat)
+        })
+
+        return {
+            name: pokemonStatsData.name,
+            health: stats[0],
+            attack: stats[1],
+            defense: stats[2],
+            speed: stats[5],
+            height: pokemonStatsData.height,
+            weight: pokemonStatsData.weight,
+            created: false
+        }
+        
+    })
+
+    return Promise.all(pokemons)
+   
+}
+
+const getTypes = async() => {
+    const urlTypes = await axios.get('https://pokeapi.co/api/v2/type')
+    const data = await urlTypes.data
+    const types = []
+
+    data.results.map(e => {
+        types.push(e.name)
+    })
+
+    return types
+}
+
+router.get('/pokemons', async(req, res) => {
+    const pokemonsTotal = await getAllPokemons()
+    const havePokemons = await Pokemon.findAll()
+    console.log(havePokemons.length)
+    //return res.json(pokemonsTotal)
+       try {
+        if(havePokemons.length === 0){
+            console.log('dentro del if')
+            await pokemonsTotal.map(async (e) => {
+                await Pokemon.findOrCreate({
+                    where:{
+
+                        name: e.name,
+                        health: e.health,
+                        attack: e.attack,
+                        defense: e.defense,
+                        speed: e.speed,
+                        height: e.height,
+                        weight: e.weight,
+                        created: e.created
+                    }
+                })
+            })
+            console.log('ok')
+        }else{
+            console.log('pipipi')
+            return res.json(havePokemons);
+        }
+    } catch (error) {
+        res.send(error)
+    }   
+})
+
+router.get('/types', async(req, res) => {
+    
+})
 
 module.exports = router;
